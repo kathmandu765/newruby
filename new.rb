23aeb47a617b93934d50a20dc92454f8da66d19e
kathmandu765 <69729061+kathmandu765@users.noreply.github.com>
2022-12-21 02:02:33 +0545

    commit

---
+++ new.rb
@@ -0,0 +1,17 @@
+# name = 'Biraj'
+# puts name
+message = 'I am learning ruby and I am on my day 4'
+# puts message.length -- it counts the lenght word
+# puts message[0]
+# puts message[-1]
+# puts message[0,4]
+# puts message[5..20] -- it gives from 5 to 20
+# puts message.slice(8,17) -- it gives the sliced value
+# puts message.split -- arrays of words
+# puts message.chars -- array of chars
+# puts message.count("a") -- it counts
+# puts message.upcase -- it makes all up case
+# puts message.downcase -- it makes all down case
+# puts message.capitalize -- it captilize the start
+# puts message.swapcase -- it makes th first letter/word small and rest captilize
+# puts message.chop -- it exclude the last word
